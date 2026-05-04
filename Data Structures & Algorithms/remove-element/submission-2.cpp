9bf9e8a5b20c35a4fd00adacf6bdd15816b2ce0a
Adarsh Pakala <82712427+adarshpakala@users.noreply.github.com>
2026-05-03 19:45:31 -0700

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+
+
+        // vector <int> res;
+
+        // for ( int i : nums){
+        //     if (i != val){
+
+        //         res.push_back(i);
+        //     }
+        // }
+        // for (int j =0; j<res.size(); j++){
+
+        //     nums[j] = res[j];
+        // }
+        //     return res.size();
+
+        int k =0;
+
+        for ( int i =0; i< nums.size();i++){
+            if (nums[i]!= val){
+                nums[k++]=nums[i];
+            }
+        }
+
+        return k;
+            
+            
+            } 
+        
+        
+    
+};
